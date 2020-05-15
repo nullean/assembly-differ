eebea87335936247153ea7d54ff639ff8bcec7ea
Martijn Laarman <Mpdreamz@gmail.com>
2020-05-15 13:56:06 +0200

    Sync build across projects (#14)

--- build/scripts/CommandLine.fs
+++ build/scripts/CommandLine.fs
@@ -5,12 +5,15 @@ open Microsoft.FSharp.Reflection
 
 type Arguments =
     | [<CliPrefix(CliPrefix.None);SubCommand>] Clean
-    | [<CliPrefix(CliPrefix.None);SubCommand>] Build 
+    | [<CliPrefix(CliPrefix.None);SubCommand>] Build
+    
     | [<CliPrefix(CliPrefix.None);Hidden;SubCommand>] PristineCheck 
     | [<CliPrefix(CliPrefix.None);Hidden;SubCommand>] GeneratePackages
     | [<CliPrefix(CliPrefix.None);Hidden;SubCommand>] ValidatePackages 
     | [<CliPrefix(CliPrefix.None);Hidden;SubCommand>] GenerateReleaseNotes 
-    | [<CliPrefix(CliPrefix.None);SubCommand>] Release 
+    | [<CliPrefix(CliPrefix.None);Hidden;SubCommand>] GenerateApiChanges 
+    | [<CliPrefix(CliPrefix.None);SubCommand>] Release
+    
     | [<CliPrefix(CliPrefix.None);Hidden;SubCommand>] CreateReleaseOnGithub 
     | [<CliPrefix(CliPrefix.None);SubCommand>] Publish
     
@@ -27,10 +30,12 @@ with
             
             | SingleTarget _ -> "Runs the provided sub command without running their dependencies"
             | Token _ -> "Token to be used to authenticate with github"
+            
             | PristineCheck  
             | GeneratePackages
             | ValidatePackages 
             | GenerateReleaseNotes
+            | GenerateApiChanges
             | CreateReleaseOnGithub 
                 -> "Undocumented, dependent target"
     member this.Name =
